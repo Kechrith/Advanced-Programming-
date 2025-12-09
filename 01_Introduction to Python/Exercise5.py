1ee2e6843c09ab982691139cd77403129906add5
Tang Kechrith <tangkechrith@gmail.com>
2025-12-09 14:25:26 +0700

    update

--- 01_Introduction to Python/Exercise5.py
+++ 01_Introduction to Python/Exercise5.py
@@ -1,6 +1,7 @@
-""" 
-This Program Ask user to guess the number by enter the number
-"""
+""" Create  a  variable  called  computer_num  and  set  the  value  to  50.  Ask  the  user  to  enter  a  number. 
+While their guess is not the same as the computer_num value,  tell them if their guess is too low or 
+too high and ask them to have another guess. If they enter the same value as computer_num, display 
+the message “Well done, you took [count] attempts”. """
 
 # Variable 
 computer_num = 50 

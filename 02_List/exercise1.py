e2872668765c31fd04cfe48c8e2e2d1713b54057
Vannara22 <vannaraniem2@gmail.com>
2025-12-16 15:05:24 +0700

    update

---
+++ 02_List/exercise1.py
@@ -0,0 +1,18 @@
+# group 9 
+# Write a program to store a integers and no duplicate 
+
+# create 2 list
+list1= []
+list2 = []
+
+while True:
+    user = input("Please Enter number (Press 0 to stop): ").strip()
+
+    # Condition
+    if user == '0':
+        break
+    
+    elif 
+        list1.append(user)
+
+    print(list1)

077c8a2ef95fa2c866b64a0c3fc6a566f8444a50
Vannara22 <vannaraniem2@gmail.com>
2025-12-16 16:48:37 +0700

    update

---
+++ 02_List/exercise2.py
@@ -0,0 +1,32 @@
+# Group 9
+# write a program to store the names of comstuer who join your party
+
+# Store their names in the list
+name_list = []
+
+# check the duplicate 
+name_initial = input("Enter three names of people you want to invite (using comma): ").strip()
+for name in name_initial.split(','):
+    cleaned_name = name.strip()
+    if cleaned_name and cleaned_name not in name_list:
+        name_list.append(name)
+    
+while True:
+    add_name = input("Would you want to add other people to join? Yes/NO: ").lower()
+
+    if add_name == 'no':
+        break
+    elif add_name == 'yes':
+        new_name = input ("Enter people you want to invite: ").strip()
+        if new_name and new_name not in name_list:
+            name_list.append(new_name)
+    else:
+        print("Please Enter Yes or No!!")
+    
+# Display The Names
+print('=' * 35)
+print("The Name that You invited to the party:")
+for index, name in enumerate(name_list, 1):
+    print(f"{index}.{name.strip()}")
+
+    

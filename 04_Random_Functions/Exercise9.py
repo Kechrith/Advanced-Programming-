f2f8064df60da28e712b4cfe639d08c70152b728
Tang Kechrith <tangkechrith@gmail.com>
2026-01-04 20:00:44 +0700

    bos bro hith

--- 04_Random_Functions/Exercise9.py
+++ 04_Random_Functions/Exercise9.py
@@ -1,7 +1,27 @@
 import random 
 
-pearl = ["W", "W", "W", "B", "B", "B"]
+pearls = ["W", "W", "W", "B", "B", "B"]
 
-choice = [
-    
+choices = [
+    (["W"], ["W", "W", "B", "B", "B"]),
+    (["B"], ["W", "W", "W", "B", "B"]),
+    (["W", "W"], ["W", "B", "B", "B"]),
+    (["W", "B"], ["W", "W", "B", "B"]),
+    (["B", "B"], ["W", "W", "W", "B"]),
+    (["W", "W", "W"], ["B", "B", "B"])
 ]
+
+choice_number = 1
+
+for bowl1, bowl2 in choices:
+    survival = 0
+    for i in range(100):
+        bowl = random.choice([bowl1, bowl2])
+        pearl = random.choice(bowl)
+
+        if pearl == "W":
+            survival += 1
+
+    print("Choice", choice_number, ":", bowl1, "and", bowl2, ", the number of survival is", survival, "/ 100 times")
+
+    choice_number += 1

22614b02b32848e50b429082110a21de322168cd
Vannara22 <vannaraniem2@gmail.com>
2026-02-23 21:35:24 +0700

    Vannara

---
+++ 07_Lambda Function/exercise4.py
@@ -0,0 +1,17 @@
+# Exercise 4
+"""
+4. Concatenate corresponding items of two lists of strings. 
+- Input: list1 = ["Hello", "Good"] and list2 = ["World", "Morning"] 
+- Output: ["Hello World", "Good Morning"]
+"""
+
+# Define the first list
+list1 = ["Hello", "Good"]
+
+# Define the second list
+list2 = ["World", "Morning"]
+
+result = list(map(lambda x, y: x + " " + y, list1, list2))
+
+# Display the result
+print(result)
